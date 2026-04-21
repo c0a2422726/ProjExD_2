e287a061fb420f93c025e8943b984dc3f564c014
Ryousei <c0a2422726@edu.teu.ac.jp>
2026-04-21 14:29:03 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -20,6 +21,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -28,23 +39,24 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
-        key_lst = pg.key.get_pressed()
+        key_lst = pg.key.get_pressed()        
+        sum_mv = [0, 0]
+#        if key_lst[pg.K_UP]:
+#            sum_mv[1] -= 5
+#        if key_lst[pg.K_DOWN]:
+#            sum_mv[1] += 5
+#        if key_lst[pg.K_LEFT]:
+#            sum_mv[0] -= 5
+#        if key_lst[pg.K_RIGHT]:
+#            sum_mv[0] += 5
         for key,mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-        
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
